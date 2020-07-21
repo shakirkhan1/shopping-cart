1300ed27cc140434d97f6f80066040d56e87f4a9
shakirkhan1 <me.shakir1996@gmail.com>
2020-07-21 10:39:50 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
       </header>
+      <main>
+        product list
+      </main>
+      <footer>
+        All right reserved.
+      </footer>
     </div>
   );
 }
