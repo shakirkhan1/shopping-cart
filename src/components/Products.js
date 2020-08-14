c16e9c46b9bcc51b29d3de7c3d54ac9f1e6bfc73
shakirkhan1 <me.shakir1996@gmail.com>
2020-08-14 09:26:27 +0530

    added redux filter and updated app.js file

--- src/components/Products.js
+++ src/components/Products.js
@@ -92,6 +92,6 @@ class Products extends Component {
       );
     }
   }
-  export default connect((state) => ({ products: state.products.items }), {
+  export default connect((state) => ({ products: state.products.filteredItems }), {
     fetchProducts,
   })(Products);
